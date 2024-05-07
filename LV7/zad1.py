c04e0af921fca6fa48948f6571fd404c71b08b32
syned0906 <156336061+syned0906@users.noreply.github.com>
2024-05-07 09:14:51 +0200

    Create zad1.py

---
+++ LV7/zad1.py
@@ -0,0 +1,66 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.cluster.hierarchy import dendrogram
+from sklearn.datasets import make_blobs, make_circles, make_moons
+from sklearn.cluster import KMeans, AgglomerativeClustering
+
+
+def generate_data(n_samples, flagc):
+    # 3 grupe
+    # dobro ih podijeli
+    if flagc == 1:
+        random_state = 365
+        X,y = make_blobs(n_samples=n_samples, random_state=random_state)
+    
+    # 3 grupe
+    # uglavnom ih dobro podijeli
+    elif flagc == 2:
+        random_state = 148
+        X,y = make_blobs(n_samples=n_samples, random_state=random_state)
+        transformation = [[0.60834549, -0.63667341], [-0.40887718, 0.85253229]]
+        X = np.dot(X, transformation)
+
+    # 4 grupe
+    # # uglavnom ih dobro podijeli 
+    elif flagc == 3:
+        random_state = 148
+        X, y = make_blobs(n_samples=n_samples,
+                        centers = 4,
+                        cluster_std=np.array([1.0, 2.5, 0.5, 3.0]),
+                        random_state=random_state)
+    # 2 grupe
+    # ne podijeli ih dobro
+    elif flagc == 4:
+        X, y = make_circles(n_samples=n_samples, factor=.5, noise=.05)
+    
+    # 2 grupe  
+    # ne podijeli ih dobro
+    elif flagc == 5:
+        X, y = make_moons(n_samples=n_samples, noise=.05)
+    
+    else:
+        X = []
+        
+    return X
+
+# generiranje podatkovnih primjera
+X = generate_data(500, 4)
+
+# prikazi primjere u obliku dijagrama rasprsenja
+plt.figure()
+plt.scatter(X[:,0],X[:,1])
+plt.xlabel('$x_1$')
+plt.ylabel('$x_2$')
+plt.title('podatkovni primjeri')
+plt.show()
+
+km = KMeans(n_clusters=2, init='random')
+km.fit(X)
+labels = km.predict(X)
+
+plt.figure()
+plt.scatter(X[:,0],X[:,1], c=labels)
+plt.xlabel('$x_1$')
+plt.ylabel('$x_2$')
+plt.title('podatkovni primjeri')
+plt.show()
