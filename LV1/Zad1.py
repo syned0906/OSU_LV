826b24c89456efd9b500060522bb7df5e83524a0
syned0906 <156336061+syned0906@users.noreply.github.com>
2024-05-07 09:02:43 +0200

    Add files via upload

---
+++ LV1/Zad1.py
@@ -0,0 +1,6 @@
+def total_euro(work_hours,hourly_pay):
+   print(work_hours*hourly_pay)
+ 
+work_hours=int(input('Unesite broj radnih sati:'))
+hourly_pay=float(input('Unesite satnicu:'))
+total_euro(work_hours,hourly_pay)
